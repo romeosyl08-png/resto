37d7041ecb31672c926a2b2719cd17370387dabc
romeosyl08-png <romeosyl08@gmail.com>
2025-12-31 21:42:59 +0000

    Add MealForm for Meal model in forms.py
    Created a form for the Meal model with specific fields and widgets.

---
+++ resto/staff/forms.py
@@ -0,0 +1,11 @@
+# staff/forms.py
+from django import forms
+from shop.models import Meal
+
+class MealForm(forms.ModelForm):
+    class Meta:
+        model = Meal
+        fields = ["category", "name", "slug", "description", "price", "stock", "max_per_order", "is_active", "image"]
+        widgets = {
+            "description": forms.Textarea(attrs={"rows": 3}),
+        }
