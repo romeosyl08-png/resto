f7964e5e698beeeaff858655f2316f8093f50d63
romeosyl08-png <romeosyl08@gmail.com>
2026-01-01 21:14:23 +0000

    Update urls.py

--- resto/shop/urls.py
+++ resto/shop/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.meal_llist, name='meal_list'),
+    path('', views.meal_list, name='meal_list'),
     path('meal/<slug:slug>/', views.meal_detail, name='meal_detail'),
 ]
 
