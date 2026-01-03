1a3cb600ff3d686dbcc2ea061f2f790cc8194ccc
romeosyl08-png <romeosyl08@gmail.com>
2026-01-03 07:37:42 +0000

    Update forms.py

--- resto/orders/forms.py
+++ resto/orders/forms.py
@@ -4,5 +4,5 @@ from comptes.models import UserProfile
 
 class CheckoutForm(forms.Form):
     customer_name = forms.CharField(label="Nom", max_length=150)
-    phone = forms.CharField(label="Téléphone", max_length=30)
+    phone = forms.CharField(label="Téléphone", max_length=10)
     address = forms.CharField(label="Adresse / Lieu de livraison", widget=forms.Textarea)
