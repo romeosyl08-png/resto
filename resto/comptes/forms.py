6ca0f86354cef79053f7356fada03bcd36402684
romeosyl08-png <romeosyl08@gmail.com>
2026-01-03 08:26:53 +0000

    Update forms.py

--- resto/comptes/forms.py
+++ resto/comptes/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from .models import UserProfile
 
 class ProfileForm(forms.ModelForm):
+    address = forms.CharField(
+        label="Adresse / Lieu de livraison",
+        required=False,
+        widget=forms.Textarea(attrs={
+            "class": "form-control",
+            "rows": 3,
+            "readonly": "readonly"
+        })
+    )
+
     class Meta:
         model = UserProfile
-        fields = ['full_name', 'phone', 'address']
+        fields = ["customer_name", "phone", "address"]
