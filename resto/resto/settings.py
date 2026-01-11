5bc5369fcefddf5f9393cceac989ffbe5f95d02d
romeosyl08-png <romeosyl08@gmail.com>
2026-01-11 04:53:16 +0000

    perfect perfect 2.1

--- resto/resto/settings.py
+++ resto/resto/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o^c!p2#f_n6nl8z0xhk%6hn=cz$y#ijx)jh^1wq*6!9wo7bht1'
+SECRET_KEY = os.environ.get("SECRET_KEY"),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
