2989f1ffeabd90bc31e00ea80c477c2580f8d0e0
romeosyl08-png <romeosyl08@gmail.com>
2026-01-13 19:28:47 +0000

    Update utils.py

--- resto/shop/utils.py
+++ resto/shop/utils.py
@@ -5,7 +5,6 @@ CUTOFF_TIME = time(9, 30)
 
 def is_order_window_open(now_time: time) -> bool:
     # ouvert de 18:00 → 23:59 et de 00:00 → 09:29
-    print("CHECK:", now_time, "OPEN:", OPEN_TIME, "CUTOFF:", CUTOFF_TIME)
     return (now_time >= OPEN_TIME) or (now_time < CUTOFF_TIME)
 
 def service_date(now_dt):
